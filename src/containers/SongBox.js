5c0e1aa327a6c0d19149e69dd6009afab3696295
Davide Campagna <gdavidecampagna@gmail.com>
2019-10-29 17:55:11 +0000

    fetch data

--- src/containers/SongBox.js
+++ src/containers/SongBox.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import SongList from '../components/SongList'
 
 class SongBox extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      songs: [],
+      songNames: []
+    }
+  }
+
+  componentDidMount() {
+    const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json";
+
+    fetch(url)
+      .then(res => res.json())
+      .then(songs => {
+        const songNames = songs.map(song => song.feed);
+        const uniqueSongNames = Array.from(new Set(songNames))
+        this.setState({
+          songs: songs,
+          songNames: uniqueSongNames
+        })
+      })
+      .catch(err => console.error(err))
+  }
 
   render(){
     return(
-      <div>
-        <SongList />
+      <div className="song-box">
+        <SongList songs={this.state.songNames}/>
       </div>
     )
   }
